5b79c8c6ec1277a9050b74101ad24823a5b32a24
gwhan <dolewoo@gmail.com>
2018-12-01 21:45:15 +0900

    first local commit to master

---
+++ jenkinsdocker/src/main/java/com/jenkins/docker/JenkinsdockerApplication.java
@@ -0,0 +1,12 @@
+package com.jenkins.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsdockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsdockerApplication.class, args);
+	}
+}
